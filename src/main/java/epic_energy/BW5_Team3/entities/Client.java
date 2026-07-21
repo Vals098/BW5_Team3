3c08c231b7d73accfcfb1bb522e989104411dd43
PaoloF16 <paolo.cafis@gmail.com>
2026-07-21 13:40:35 +0200

    f/ created entities

--- src/main/java/epic_energy/BW5_Team3/entities/Client.java
+++ src/main/java/epic_energy/BW5_Team3/entities/Client.java
@@ -1,9 +1,11 @@
 package epic_energy.BW5_Team3.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Setter;
 
+import java.util.HashSet;
+import java.util.Set;
 import java.util.UUID;
 
 @Entity
@@ -11,6 +13,34 @@ public class Client {
 
     @Id
     @GeneratedValue
+    @Setter(AccessLevel.NONE)
     private UUID clientId;
 
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String surname;
+
+    @Column(nullable = false)
+    private String avatar = "https://placecats.com/300/200";
+
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "employee_role",
+            joinColumns = @JoinColumn(name = "employee_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles = new HashSet<>();
 }
+
