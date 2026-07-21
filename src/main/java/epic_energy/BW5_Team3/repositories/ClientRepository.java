bd9790d38e9f37c9cf87b914e2af3e0a5e343b4c
Vals098 <valeria.farinosi@gmail.com>
2026-07-21 18:55:54 +0200

    fix: RoleRepository/ClientRepository

--- src/main/java/epic_energy/BW5_Team3/repositories/ClientRepository.java
+++ src/main/java/epic_energy/BW5_Team3/repositories/ClientRepository.java
@@ -1,7 +1,6 @@
 package epic_energy.BW5_Team3.repositories;
 
 import epic_energy.BW5_Team3.entities.Client;
-import epic_energy.BW5_Team3.entities.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
