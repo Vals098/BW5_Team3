a5d85c41be3e27100e8b72951ac3394b63032a64
Vals098 <valeria.farinosi@gmail.com>
2026-07-21 10:26:52 +0200

    Initial commit

---
+++ src/main/java/epic_energy/BW5_Team3/Bw5Team3Application.java
@@ -0,0 +1,13 @@
+package epic_energy.BW5_Team3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bw5Team3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bw5Team3Application.class, args);
+	}
+
+}
---
+++ src/test/java/epic_energy/BW5_Team3/Bw5Team3ApplicationTests.java
@@ -0,0 +1,13 @@
+package epic_energy.BW5_Team3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bw5Team3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
