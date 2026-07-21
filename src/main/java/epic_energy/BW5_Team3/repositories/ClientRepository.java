f7ce4282a17a0169cad75f4ceb927d2b0ce0183f
Vals098 <valeria.farinosi@gmail.com>
2026-07-21 17:10:28 +0200

    add: ClientRepository with base methods

--- src/main/java/epic_energy/BW5_Team3/repositories/ClientRepository.java
+++ src/main/java/epic_energy/BW5_Team3/repositories/ClientRepository.java
@@ -4,10 +4,17 @@ import epic_energy.BW5_Team3.entities.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
-//esempio di repo solo per postare i packages, da sviluppare
 
 @Repository
 public interface ClientRepository extends JpaRepository<Client, UUID> {
+
+    Optional<Client> findByIva(String iva);
+
+    Optional<Client> findByEmail(String email);
+
+    Optional<Client> findByPec(String pec);
+
 }
